f906e12ccd5a57801903a3b0d15ace8877a8ab01
Luiz Phelipe Soares Rodrigues <lphelipe@c2r2p4.42sp.org.br>
2022-07-16 19:21:06 -0300

    Zerando a matriz antes de começar e imprimindo o resultado

--- main.c
+++ main.c
@@ -6,53 +6,92 @@
 /*   By: lphelipe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 15:27:57 by lphelipe          #+#    #+#             */
-/*   Updated: 2022/07/16 18:08:19 by lphelipe         ###   ########.fr       */
+/*   Updated: 2022/07/16 19:18:47 by lphelipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
-void	assign_parameters(int h[4][2], int v[4][2], char *argv[]);
-void	play(int h[4][2], int v[4][2]);
+void	assign_parameters(int h[4][2], int v[4][2], char p[16]);
+void	play(int h[4][2], int v[4][2], int a[4][4]);
 void	answer_h_1_4_or_4_1(int h[4][2],int a[4][4]);
 void	answer_v_1_4_or_4_1(int v[4][2], int a[4][4]);
 void	define_h_answer(int a[4], int p1, int p2, int p3, int p4);
+void	convert_argv_to_char_array(char *argv[], char p[16]);
+void	print_answer(int a[4][4]);
+void	clean_matrix(int a[4][4]);
 
 int main(int argc, char *argv[])
 {
 	int h[4][2];
 	int v[4][2];
+	char p[16];
+	int a[4][4];
 
-	assign_parameters(h, v, argv);
-
-	play(h, v);
+	convert_argv_to_char_array(argv, p);
+	assign_parameters(h, v, p);
+	clean_matrix(a);
+	play(h, v, a);
+	print_answer(a);
+	
+	printf("\n A maldita posição a[2][3] é igual a %d\n\n", a[2][3]);
 }
 
-void	assign_parameters(int h[4][2], int v[4][2], char *argv[])
+void	clean_matrix(int a[4][4])
 {
-	v[0][0] = *argv[1] - '0';
-	v[1][0] = *argv[2] - '0';
-	v[2][0] = *argv[3] - '0';
-	v[3][0] = *argv[4] - '0';
-	v[0][1] = *argv[5] - '0';
-	v[1][1] = *argv[6] - '0';
-	v[2][1] = *argv[7] - '0';
-	v[3][1] = *argv[8] - '0';
-	h[0][0] = *argv[9] - '0';
-	h[1][0] = *argv[10] - '0';
-	h[2][0] = *argv[11] - '0';
-	h[3][0] = *argv[12] - '0';
-	h[0][1] = *argv[13] - '0';
-	h[1][1] = *argv[14] - '0';
-	h[2][1] = *argv[15] - '0';
-	h[3][1] = *argv[16] - '0';
-}
-
-void	play(int h[4][2], int v[4][2])
-{
-	int	a[4][4];
 	int	i;
+	int	j;
 
+	i = 0;
+	while (i < 4)
+	{
+		j = 0;
+		while (j < 4)
+		{
+			a[i][j] = 0;
+			j++;
+		}
+		i++;
+	}
+}
+
+void	convert_argv_to_char_array(char *argv[], char p[16])
+{
+	int i;
+	char *arg;
+	
+	arg = argv[1];
+	i = 0;
+	while(i < 16)
+	{
+		p[i] = *arg;
+		arg = arg + 2;
+		i++;
+	}
+}
+
+void	assign_parameters(int h[4][2], int v[4][2], char p[16])
+{
+	v[0][0] = p[0] - '0';
+	v[1][0] = p[1] - '0';
+	v[2][0] = p[2] - '0';
+	v[3][0] = p[3] - '0';
+	v[0][1] = p[4] - '0';
+	v[1][1] = p[5] - '0';
+	v[2][1] = p[6] - '0';
+	v[3][1] = p[7] - '0';
+	h[0][0] = p[8] - '0';
+	h[1][0] = p[9] - '0';
+	h[2][0] = p[10] - '0';
+	h[3][0] = p[11] - '0';
+	h[0][1] = p[12] - '0';
+	h[1][1] = p[13] - '0';
+	h[2][1] = p[14] - '0';
+	h[3][1] = p[15] - '0';
+}
+
+void	play(int h[4][2], int v[4][2], int a[4][4])
+{
 	printf("iniciando jogo\n");
 
 	answer_h_1_4_or_4_1(h, a);
