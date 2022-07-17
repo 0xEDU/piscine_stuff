9e8c2689f2c1b41effe3bae3a598fe14944a6b55
Eduardo Tachotte <etachott@c2r9p4.42sp.org.br>
2022-07-17 20:00:04 -0300

    cheque de erros corrigidos

--- check_errors.c
+++ check_errors.c
@@ -6,7 +6,7 @@
 /*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/17 16:06:48 by etachott          #+#    #+#             */
-/*   Updated: 2022/07/17 18:33:46 by etachott         ###   ########.fr       */
+/*   Updated: 2022/07/17 19:57:17 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -21,14 +21,14 @@ int	check_errors(char p[16], char *argv[], int argc);
 
 int	check_errors(char p[16], char *argv[], int argc)
 {
-	if (detect_error(p) == 1)
-		return (1);
-	if (count_argc(argc) == 1)
-		return (1);
-	if (count_args(argv) == 1)
-		return (1);
-	else
+	if (detect_error(p) == 0)
 		return (0);
+	if (count_argc(argc) == 0)
+		return (0);
+	if (count_args(argv) == 0)
+		return (0);
+	else
+		return (1);
 }
 
 int	count_argc(int argc)
@@ -36,9 +36,9 @@ int	count_argc(int argc)
 	if (argc != 2)
 	{
 		write(1, "Error\n", 6);
-		return (1);
+		return (0);
 	}
-	return (0);
+	return (1);
 }
 
 int	count_args(char *argv[])
@@ -56,9 +56,9 @@ int	count_args(char *argv[])
 	if (j != 31)
 	{
 		write(1, "Error\n", 6);
-		return (1);
+		return (0);
 	}
-	return (0);
+	return (1);
 }
 
 int	detect_error(char p[16])
@@ -76,9 +76,9 @@ int	detect_error(char p[16])
 		else
 		{
 			write(1, "Error\n", 6);
-			return (1);
+			return (0);
 		}
 		i++;
 	}
-	return (0);
+	return (1);
 }
