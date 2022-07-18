595a44cec1711e51690140ae0773fb8cd47d6cbd
Eduardo Frangiotti <efrangio@c2r9p5.42sp.org.br>
2022-07-17 21:11:26 -0300

    tirando funcoes grandes do additional_combinations antes de subir pra 8A

--- additional_combinations.c
+++ additional_combinations.c
@@ -6,9 +6,18 @@
 /*   By: efrangio <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/17 10:49:07 by efrangio          #+#    #+#             */
-/*   Updated: 2022/07/17 18:00:12 by efrangio         ###   ########.fr       */
+/*   Updated: 2022/07/17 21:10:39 by efrangio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+void	answer_v_2_3_additionals_1_2_3(int v[4][2], int a[4][4], int i);
+void	answer_v_3_2_additionals_1_2_3(int v[4][2], int a[4][4], int i);
+void	answer_h_2_3_additionals_1_2_3(int h[4][2], int a[4][4], int i);
+void	answer_h_3_2_additionals_1_2_3(int h[4][2], int a[4][4], int i);
+void	answer_h_1_3_additionals_1_2_3(int h[4][2], int a[4][4], int i);
+void	answer_h_3_1_additionals_1_2_3(int h[4][2], int a[4][4], int i);
+void	answer_v_1_3_additionals_1_2_3(int v[4][2], int a[4][4], int i);
+void	answer_v_3_1_additionals_1_2_3(int v[4][2], int a[4][4], int i);
 
 // verifica as combinações adicionais possíveis de 2 e 3 vertical
 // 1, 2 e 3 na primeira casa
@@ -24,22 +33,7 @@ void	answer_v_2_3_additionals(int v[4][2], int a[4][4])
 	i = 0;
 	while (i < 4)
 	{
-		//printf("Verificando coluna %d\n", i);
-		if ((v[i][0] == 2 && v[i][1] == 3) && a[0][i] == 1)
-		{
-			a[2][i] = 3;
-			a[3][i] = 2;
-		}
-		if ((v[i][0] == 2 && v[i][1] == 3) && a[0][i] == 2)
-		{
-			a[2][i] = 3;
-			a[3][i] = 1;
-		}
-		if ((v[i][0] == 2 && v[i][1] == 3) && a[0][i] == 3)
-		{
-			a[2][i] = 2;
-			a[3][i] = 1;
-		}
+		answer_v_2_3_additionals_1_2_3(v, a, i);
 		if ((v[i][0] == 2 && v[i][1] == 3) && a[3][i] == 2)
 		{
 			a[2][i] = 3;
@@ -52,6 +46,25 @@ void	answer_v_2_3_additionals(int v[4][2], int a[4][4])
 		}
 		i++;
 	}
+}
+
+void	answer_v_2_3_additionals_1_2_3(int v[4][2], int a[4][4], int i)
+{
+	if ((v[i][0] == 2 && v[i][1] == 3) && a[0][i] == 1)
+	{
+		a[2][i] = 3;
+		a[3][i] = 2;
+	}
+	if ((v[i][0] == 2 && v[i][1] == 3) && a[0][i] == 2)
+	{
+		a[2][i] = 3;
+		a[3][i] = 1;
+	}
+	if ((v[i][0] == 2 && v[i][1] == 3) && a[0][i] == 3)
+	{
+		a[2][i] = 2;
+		a[3][i] = 1;
+	}	
 }
 
 // verifica as combinações adicionais possíveis de 3 e 2 vertical
@@ -67,22 +80,8 @@ void	answer_v_3_2_additionals(int v[4][2], int a[4][4])
 
 	i = 0;
 	while (i < 4)
-	{
-		if ((v[i][0] == 3 && v[i][1] == 2) && a[3][i] == 1)
-		{
-			a[0][i] = 2;
-			a[1][i] = 3;
-		}
-		if ((v[i][0] == 3 && v[i][1] == 2) && a[3][i] == 2)
-		{
-			a[0][i] = 1;
-			a[1][i] = 3;
-		}
-		if ((v[i][0] == 3 && v[i][1] == 2) && a[3][i] == 3)
-		{
-			a[0][i] = 1;
-			a[1][i] = 2;
-		}
+	{	
+		answer_v_3_2_additionals_1_2_3(v, a, i);
 		if ((v[i][0] == 3 && v[i][1] == 2) && a[0][i] == 2)
 		{
 			a[1][i] = 3;
@@ -96,6 +95,27 @@ void	answer_v_3_2_additionals(int v[4][2], int a[4][4])
 		i++;
 	}
 }
+
+void	answer_v_3_2_additionals_1_2_3(int v[4][2], int a[4][4], int i)
+{
+	if ((v[i][0] == 3 && v[i][1] == 2) && a[3][i] == 1)
+	{
+		a[0][i] = 2;
+		a[1][i] = 3;
+	}
+	if ((v[i][0] == 3 && v[i][1] == 2) && a[3][i] == 2)
+	{
+		a[0][i] = 1;
+		a[1][i] = 3;
+	}
+	if ((v[i][0] == 3 && v[i][1] == 2) && a[3][i] == 3)
+	{
+		a[0][i] = 1;
+		a[1][i] = 2;
+	}
+	
+}
+
 
 // verifica as combinações adicionais possíveis de 2 e 3 horizontal
 // 1, 2 e 3 na primeira
@@ -111,21 +131,7 @@ void	answer_h_2_3_additionals(int h[4][2], int a[4][4])
 	i = 0;
 	while (i < 4)
 	{
-		if ((h[i][0] == 2 && h[i][1] == 3) && a[i][0] == 1)
-		{
-			a[i][2] = 3;
-			a[i][3] = 2;
-		}
-		if ((h[i][0] == 2 && h[i][1] == 3) && a[i][0] == 2)
-		{
-			a[i][2] = 3;
-			a[i][3] = 1;
-		}
-		if ((h[i][0] == 2 && h[i][1] == 3) && a[i][0] == 3)
-		{
-			a[i][2] = 2;
-			a[i][3] = 1;
-		}
+		answer_h_2_3_additionals_1_2_3(h, a, i);
 		if ((h[i][0] == 2 && h[i][1] == 3) && a[i][2] == 2)
 		{
 			a[i][0] = 3;
@@ -140,6 +146,24 @@ void	answer_h_2_3_additionals(int h[4][2], int a[4][4])
 	}
 }
 
+void	answer_h_2_3_additionals_1_2_3(int h[4][2], int a[4][4], int i)
+{
+	if ((h[i][0] == 2 && h[i][1] == 3) && a[i][0] == 1)
+	{
+		a[i][2] = 3;
+		a[i][3] = 2;
+	}
+	if ((h[i][0] == 2 && h[i][1] == 3) && a[i][0] == 2)
+	{
+		a[i][2] = 3;
+		a[i][3] = 1;
+	}
+	if ((h[i][0] == 2 && h[i][1] == 3) && a[i][0] == 3)
+	{
+		a[i][2] = 2;
+		a[i][3] = 1;
+	}
+}
 // verifica as combinações adicionais possíveis de 3 e 2 horizontal
 // 1, 2 e 3 na última casa
 // 2 na primeira ou segunda casa
@@ -154,21 +178,7 @@ void	answer_h_3_2_additionals(int h[4][2], int a[4][4])
 	i = 0;
 	while (i < 4)
 	{
-		if ((h[i][0] == 3 && h[i][1] == 2) && a[i][3] == 1)
-		{
-			a[i][0] = 2;
-			a[i][1] = 3;
-		}
-		if ((h[i][0] == 3 && h[i][1] == 2) && a[i][3] == 2)
-		{
-			a[i][0] = 1;
-			a[i][1] = 3;
-		}
-		if ((h[i][0] == 3 && h[i][1] == 2) && a[i][3] == 3)
-		{
-			a[i][0] = 1;
-			a[i][1] = 2;
-		}
+		answer_h_3_2_additionals_1_2_3(h, a, i);
 		if ((h[i][0] == 3 && h[i][1] == 2) && a[i][0] == 2)
 		{
 			a[i][1] = 3;
@@ -180,6 +190,25 @@ void	answer_h_3_2_additionals(int h[4][2], int a[4][4])
 			a[i][3] = 3;
 		}
 		i++;
+	}
+}
+
+void	answer_h_3_2_additionals_1_2_3(int h[4][2], int a[4][4], int i)
+{
+	if ((h[i][0] == 3 && h[i][1] == 2) && a[i][3] == 1)
+	{
+		a[i][0] = 2;
+		a[i][1] = 3;
+	}
+	if ((h[i][0] == 3 && h[i][1] == 2) && a[i][3] == 2)
+	{
+		a[i][0] = 1;
+		a[i][1] = 3;
+	}
+	if ((h[i][0] == 3 && h[i][1] == 2) && a[i][3] == 3)
+	{
+		a[i][0] = 1;
+		a[i][1] = 2;
 	}
 }
 
@@ -197,21 +226,7 @@ void	answer_h_1_3_additionals(int h[4][2], int a[4][4])
 	i = 0;
 	while (i < 4)
 	{
-		if ((h[i][0] == 1 && h[i][1] == 3) && a[i][1] == 1)
-		{
-			a[i][2] = 3;
-			a[i][3] = 2;
-		}
-		if ((h[i][0] == 1 && h[i][1] == 3) && a[i][1] == 2)
-		{
-			a[i][2] = 3;
-			a[i][3] = 1;
-		}
-		if ((h[i][0] == 1 && h[i][1] == 3) && a[i][1] == 3)
-		{
-			a[i][2] = 1;
-			a[i][3] = 2;
-		}
+		answer_h_1_3_additionals_1_2_3(h, a, i);
 		if ((h[i][0] == 1 && h[i][1] == 3) && a[i][2] == 1)
 		{
 			a[i][1] = 3;
@@ -223,6 +238,25 @@ void	answer_h_1_3_additionals(int h[4][2], int a[4][4])
 			a[i][2] = 3;
 		}
 		i++;
+	}
+}
+
+void	answer_h_1_3_additionals_1_2_3(int h[4][2], int a[4][4], int i)
+{
+	if ((h[i][0] == 1 && h[i][1] == 3) && a[i][1] == 1)
+	{
+		a[i][2] = 3;
+		a[i][3] = 2;
+	}
+	if ((h[i][0] == 1 && h[i][1] == 3) && a[i][1] == 2)
+	{
+		a[i][2] = 3;
+		a[i][3] = 1;
+	}
+	if ((h[i][0] == 1 && h[i][1] == 3) && a[i][1] == 3)
+	{
+		a[i][2] = 1;
+		a[i][3] = 2;
 	}
 }
 
@@ -240,21 +274,7 @@ void	answer_h_3_1_additionals(int h[4][2], int a[4][4])
 	i = 0;
 	while (i < 4)
 	{
-		if ((h[i][0] == 3 && h[i][1] == 1) && a[i][2] == 1)
-		{
-			a[i][0] = 2;
-			a[i][1] = 3;
-		}
-		if ((h[i][0] == 3 && h[i][1] == 1) && a[i][2] == 2)
-		{
-			a[i][0] = 1;
-			a[i][1] = 3;
-		}
-		if ((h[i][0] == 3 && h[i][1] == 1) && a[i][2] == 3)
-		{
-			a[i][0] = 2;
-			a[i][1] = 1;
-		}
+		answer_h_3_1_additionals_1_2_3(h, a, i);
 		if ((h[i][0] == 3 && h[i][1] == 1) && a[i][0] == 1)
 		{
 			a[i][1] = 3;
@@ -265,9 +285,28 @@ void	answer_h_3_1_additionals(int h[4][2], int a[4][4])
 			a[i][0] = 2;
 			a[i][2] = 3;
 		}
-
-	i++;
+		i++;
 	}
+}
+
+void	answer_h_3_1_additionals_1_2_3(int h[4][2], int a[4][4], int i)
+{
+	if ((h[i][0] == 3 && h[i][1] == 1) && a[i][2] == 1)
+	{
+		a[i][0] = 2;
+		a[i][1] = 3;
+	}
+	if ((h[i][0] == 3 && h[i][1] == 1) && a[i][2] == 2)
+	{
+		a[i][0] = 1;
+		a[i][1] = 3;
+	}
+	if ((h[i][0] == 3 && h[i][1] == 1) && a[i][2] == 3)
+	{
+		a[i][0] = 2;
+		a[i][1] = 1;
+	}
+
 }
 
 // check combinations for 1 and 3 vertical question
@@ -284,21 +323,7 @@ void	answer_v_1_3_additionals(int v[4][2], int a[4][4])
 	i = 0;
 	while (i < 4)
 	{
-		if ((v[i][0] == 1 && v[i][1] == 3) && a[1][i] == 1)
-		{
-			a[2][i] = 3;
-			a[3][i] = 2;
-		}
-		if ((v[i][0] == 1 && v[i][1] == 3) && a[1][i] == 2)
-		{
-			a[2][i] = 3;
-			a[3][i] = 1;
-		}
-		if ((v[i][0] == 1 && v[i][1] == 3) && a[1][i] == 3)
-		{
-			a[2][i] = 1;
-			a[3][i] = 2;
-		}
+		answer_v_1_3_additionals_1_2_3(v, a, i);
 		if ((v[i][0] == 1 && v[i][1] == 3) && a[2][i] == 1)
 		{
 			a[1][i] = 3;
@@ -311,6 +336,26 @@ void	answer_v_1_3_additionals(int v[4][2], int a[4][4])
 		}
 		i++;
 	}
+}
+
+void	answer_v_1_3_additionals_1_2_3(int v[4][2], int a[4][4], int i)
+{
+	if ((v[i][0] == 1 && v[i][1] == 3) && a[1][i] == 1)
+	{
+		a[2][i] = 3;
+		a[3][i] = 2;
+	}
+	if ((v[i][0] == 1 && v[i][1] == 3) && a[1][i] == 2)
+	{
+		a[2][i] = 3;
+		a[3][i] = 1;
+	}
+	if ((v[i][0] == 1 && v[i][1] == 3) && a[1][i] == 3)
+	{
+		a[2][i] = 1;
+		a[3][i] = 2;
+	}
+
 }
 
 // check combinations for 3 and 1 vertical question
@@ -327,21 +372,7 @@ void	answer_v_3_1_additionals(int v[4][2], int a[4][4])
 	i = 0;
 	while (i < 4)
 	{
-		if ((v[i][0] == 3 && v[i][1] == 1) && a[2][i] == 1)
-		{
-			a[0][i] = 2;
-			a[1][i] = 3;
-		}
-		if ((v[i][0] == 3 && v[i][1] == 1) && a[2][i] == 2)
-		{
-			a[0][i] = 1;
-			a[1][i] = 3;
-		}
-		if ((v[i][0] == 3 && v[i][1] == 1) && a[2][i] == 3)
-		{
-			a[0][i] = 2;
-			a[1][i] = 1;
-		}
+		answer_v_3_1_additionals_1_2_3(v, a, i);
 		if ((v[i][0] == 3 && v[i][1] == 1) && a[0][i] == 1)
 		{
 			a[1][i] = 3;
@@ -354,4 +385,23 @@ void	answer_v_3_1_additionals(int v[4][2], int a[4][4])
 		}
 		i++;
 	}
+}
+void	answer_v_3_1_additionals_1_2_3(int v[4][2], int a[4][4], int i)
+{
+	if ((v[i][0] == 3 && v[i][1] == 1) && a[2][i] == 1)
+	{
+		a[0][i] = 2;
+		a[1][i] = 3;
+	}
+	if ((v[i][0] == 3 && v[i][1] == 1) && a[2][i] == 2)
+	{
+		a[0][i] = 1;
+		a[1][i] = 3;
+	}
+	if ((v[i][0] == 3 && v[i][1] == 1) && a[2][i] == 3)
+	{
+		a[0][i] = 2;
+		a[1][i] = 1;
+	}
+
 }
